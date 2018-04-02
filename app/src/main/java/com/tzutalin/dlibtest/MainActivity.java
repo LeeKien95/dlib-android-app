af4606586bb45f543d795277625f110487f04aa1
KienLeTrung <kienletrung.1995@gmail.com>
2018-04-03 04:14:34 +0700

    nothing much

--- app/src/main/java/com/tzutalin/dlibtest/MainActivity.java
+++ app/src/main/java/com/tzutalin/dlibtest/MainActivity.java
@@ -26,6 +26,8 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -49,10 +51,13 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -104,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
     private Handler backgroundHandler;
     private HandlerThread backgroundThread;
     private Handler dataSendingHandler;
+    private DataSendingThread backgroundSendingThread;
 
     private LayoutInflater mLayoutInflater;
     private ImageView mColorView;
@@ -115,7 +121,8 @@ public class MainActivity extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
     private final OnGetImageListener mOnGetPreviewListener = new OnGetImageListener();
-    private ArrayList<Point> landmarks = new ArrayList<Point>();
+    private ArrayList<Point> current_landmarks = new ArrayList<Point>();
+    private ArrayList<Point> base_landmarks = new ArrayList<Point>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -148,25 +155,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class GetLandmarks extends AsyncTask<OnGetImageListener, Void, ArrayList<Point>> {
-
         @Override
         protected ArrayList<Point> doInBackground(OnGetImageListener... onGetImageListeners) {
             return(onGetImageListeners[0].getCurrent_landmarks());
 //            return null;
         }
-
         @Override
         protected void onPreExecute() {
-
         }
-
         @Override
         protected void onPostExecute(ArrayList<Point> result) {
-            landmarks = result;
-            if(landmarks != null) {
+            current_landmarks = result;
+            if(current_landmarks != null) {
                 Toast.makeText(MainActivity.this, "Landmarks getting successfully.",  Toast.LENGTH_SHORT).show();
 //                Log.e(TAG, "" + landmarks.get(0).toString());
-
             } else {
                 Toast.makeText(MainActivity.this, "Landmarks getting failed.",  Toast.LENGTH_SHORT).show();
             }
@@ -274,7 +276,6 @@ public class MainActivity extends AppCompatActivity {
         inferenceThread = new HandlerThread("InferenceThread");
         inferenceThread.start();
         inferenceHandler = new Handler(inferenceThread.getLooper());
-
     }
 
     @SuppressLint("LongLogTag")
@@ -295,6 +296,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    // http request to server block of code
     private class DataSendingThread extends HandlerThread {
 
         Handler handler;
@@ -308,12 +311,112 @@ public class MainActivity extends AppCompatActivity {
             handler = new Handler(getLooper()) {
                 @Override
                 public void handleMessage(Message msg) {
+
                     // process incoming messages here
                     // this will run in non-ui/background thread
                 }
             };
+
+            //delay 3s before sending a json object to server.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    new GetLandmarks().execute(mOnGetPreviewListener);
+
+                }
+            }, 3000);
         }
     }
+
+    private void startSendingData() {
+        backgroundSendingThread = new DataSendingThread("Background Sending Data");
+        backgroundSendingThread.start();
+        dataSendingHandler = new Handler(backgroundSendingThread.getLooper());
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            person = new Person();
+            person.setName(etName.getText().toString());
+            person.setCountry(etCountry.getText().toString());
+            person.setTwitter(etTwitter.getText().toString());
+
+            return POST(urls[0],person);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    public static String POST(String url, ArrayList<Point> current_landmarks){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("name", person.getName());
+            jsonObject.accumulate("country", person.getCountry());
+            jsonObject.accumulate("twitter", person.getTwitter());
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+    //end http request block
 
 //    protected void takePicture() {
 //        if(null == cameraDevice) {
